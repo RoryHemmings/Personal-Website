3316362ea89c23044ce24931072662c2a991ed7e
RoryHemmings <rpmhemmings@gmail.com>
2021-06-15 02:46:44 -0700

    started portfolio portion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
 $(document).ready(() => {
 	$("header").load("../components/header.html");
 });
+
+const fps = 60;
+const delta = 1.0/fps;
+
+class Node {
+	constructor(x, y, z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
+
+class Graph {
+	constructor(numNodes) {
+		this.numNodes = numNodes;
+		this.nodes = [];
+		this.lines = [];
+		this.angle = 0;
+	}
+
+	draw() {
+		this.angle += 30 * delta;
+
+	}
+}
+
+function setup() {
+	let canvas = createCanvas(500, 500);
+	canvas.parent('canvas-target');
+
+	frameRate(fps);
+	noStroke();
+}
+
+function draw() {
+	background(255);
+
+	fill('red');
+	rect(0, 0, 50, 50);
+}
